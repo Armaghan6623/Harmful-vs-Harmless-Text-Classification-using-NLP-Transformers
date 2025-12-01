4e7f0719d301632df9ed7631e71c35c69917590b
Armaghan Tahir <armaghantahir415@gmail.com>
2025-12-01 09:08:31 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("NDMA Disaster Intelligence Dashboard")
+st.subheader("Prototype - v1.0")
+
+st.write("This is a simple test app deployed on Streamlit Cloud. The real NDMA dashboard will be added soon.")
+
+st.success("Streamlit App Loaded Successfully!")
